53a13a33d1824b4ca15519747200077891422207
João Vitor Alves <jvaa7476@gmail.com>
2026-06-16 21:08:40 -0300

    Prova2BIM: adiciona classe Serie

---
+++ Alunos/joao-vitor-andre-alves/PROVA2BIM/Serie.java
@@ -0,0 +1,115 @@
+package objects;
+import java.util.List;
+
+public class Serie {
+
+    private int id;
+    private String nome;
+    private String idioma;
+    private List<String> generos;
+    private double nota;
+    private String status;
+    private String dataEstreia;
+    private String dataTermino;
+    private String emissora;
+
+    public Serie() {
+    }
+
+    public Serie(int id, String nome, String idioma, List<String>generos, double nota, String status, String dataEstreia, String dataTermino, String emissora) {
+        this.id = id;
+        this.nome = nome;
+        this.idioma = idioma;
+        this.generos = generos;
+        this.nota = nota;
+        this.status = status;
+        this.dataEstreia = dataEstreia;
+        this.dataTermino = dataTermino;
+        this.emissora = emissora;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+    public String getIdioma() {
+        return idioma;
+    }
+
+    public void setIdioma(String idioma) {
+        this.idioma = idioma;
+    }
+
+
+    public List<String> getGeneros() {
+        return generos;
+    }
+
+    public void setGeneros(List<String> generos) {
+        this.generos = generos;
+    }
+
+
+    public double getNota() {
+        return nota;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+
+    public String getDataEstreia() {
+        return dataEstreia;
+    }
+
+    public void setDataEstreia(String dataEstreia) {
+        this.dataEstreia = dataEstreia;
+    }
+
+
+    public String getDataTermino() {
+        return dataTermino;
+    }
+
+    public void setDataTermino(String dataTermino) {
+        this.dataTermino = dataTermino;
+    }
+
+
+    public String getEmissora() {
+        return emissora;
+    }
+
+    public void setEmissora(String emissora) {
+        this.emissora = emissora;
+    }
+
+    @Override
+    public String toString() {
+        return nome;
+    }
+}
